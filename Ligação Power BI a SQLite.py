cfdf9152ae3d2eeb754ced2e83a5bab42e6af774
romeurf <pg45861@uminho.pt>
2025-04-29 10:51:31 +0100

    Update Ligação Power BI a SQLite.py

--- Ligação Power BI a SQLite.py
+++ Ligação Power BI a SQLite.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 
 # Conexão com o banco SQLite
-file_path = r'C:\Users\Romeu\OneDrive\Documentos\Global_Happiness.db')
+file_path = (r'C:\Users\Romeu\OneDrive\Documentos\Global_Happiness.db')
 conn = sqlite3.connect(file_path)
 # Leitura da tabela
 df = pd.read_sql_query("SELECT * FROM global_happiness", conn)
